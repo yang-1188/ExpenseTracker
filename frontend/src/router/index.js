1774c00dc9a97e38e7f13d5b63e243943a7beeae
yang <avril3145@gmail.com>
2025-10-25 02:19:24 +0800

    前後端架構建立

---
+++ frontend/src/router/index.js
@@ -0,0 +1,12 @@
+// src/router/index.js
+
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // 我們的路由將會加在這裡
+  ],
+})
+
+export default router
