3b589eb8f14ac1c6211ca10a54985eead7c1618e
yang <avril3145@gmail.com>
2025-11-15 13:41:25 +0800

    feat(frontend): 安裝= Element Plus,設定基礎router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,11 +2,31 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
 
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+
+//定義路由規則
+const routes = [
+  {
+    path: '/',
+    redirect: '/login', //重新導向
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: RegisterView,
+  },
+]
+
+//建立並匯出 Router
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    // 我們的路由將會加在這裡
-  ],
+  routes: routes,
 })
 
 export default router
