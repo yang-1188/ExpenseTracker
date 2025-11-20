7c6bf7083215b04c5f32cf37109a188e096300f8
yang <avril3145@gmail.com>
2025-11-20 19:51:27 +0800

    feat(frontend): 安裝並設定 Google Login 套件

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,9 @@ import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+//引入 Google Login 套件
+import vue3GoogleLogin from 'vue3-google-login'
+
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
@@ -16,4 +19,8 @@ app.use(createPinia())
 app.use(router)
 
 app.use(ElementPlus)
+//註冊 Google Login
+app.use(vue3GoogleLogin, {
+  clientId: '666265697749-jl109qu2lb5mv6qas1bff7issuoi67gi.apps.googleusercontent.com',
+})
 app.mount('#app')
